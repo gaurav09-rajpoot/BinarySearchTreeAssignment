db767a191e89e0ae2fb70d9415957fe50550079f
Gaurav Rajpoot <gaurav.rajpoot_cs.aiml19@gla.ac.in>
2021-04-13 23:38:06 +0530

    Insertion Of Node in a BST

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -4,12 +4,39 @@ import myinterface.BinarySearchTreeADT;
 import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
-    //complete this class
-
+    private Node root;
+    private int size;
     @Override
     public void insert(E data) {
+        implementation.Node<E> node = new implementation.Node<>(data);
+        if(root==null){
+            root = node;
+        }
+        else{
+            Node<E> temp = root;
+            Node<E> parent = null;
+            while(temp!=null){
+                parent = temp;
+                if(data.compareTo(temp.getData())<=0){
+                    temp = temp.getLeft();
+                }
+                else{
+                    temp = temp.getRight();
+                }
+            }
+            if(data.compareTo(parent.getData())<=0){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
 
+        }
+        size++;
+        System.out.println("Node inserted");
     }
+
+
 
     @Override
     public boolean search(E searchElement) {
